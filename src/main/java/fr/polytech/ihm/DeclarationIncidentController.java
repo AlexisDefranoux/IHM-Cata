f17583e56d15cc4d57541d2f7dfdab4f3cbddd5f
Camille Jullien <jc502919@mjollnir.unice.fr>
2018-02-05 17:00:08 +0100

    controllerDeclaration

---
+++ src/main/java/fr/polytech/ihm/DeclarationIncidentController.java
@@ -0,0 +1,50 @@
+package fr.polytech.ihm;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DeclarationIncidentController {
+    private static final Logger log = LoggerFactory.getLogger(HomeController.class);
+
+    @FXML
+    private TextField idTitre;
+
+    @FXML
+    private TextArea idDescription;
+
+    @FXML
+    private ComboBox<?> idCategorie;
+
+    @FXML
+    private TextField idLocalisation;
+
+    @FXML
+    private DatePicker idDate;
+
+    @FXML
+    private ComboBox<?> idHeure;
+
+    @FXML
+    private ComboBox<?> idMin;
+
+    @FXML
+    private Button idAnnuler;
+
+    @FXML
+    private Button idValider;
+
+    @FXML
+    private Slider idImportance;
+
+    public void initialize(){
+
+    }
+
+}
