a1242fdc47bcbca7a962d960f574336ab910f27d
Theos Mariani <theos.mariani@etu.unice.fr>
2018-02-12 16:09:04 +0100

    Ajout d'evenement

--- src/main/java/fr/polytech/ihm/DeclarationIncidentController.java
+++ src/main/java/fr/polytech/ihm/DeclarationIncidentController.java
@@ -1,14 +1,25 @@
 package fr.polytech.ihm;
 
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.awt.*;
+import java.io.IOException;
 
 public class DeclarationIncidentController {
     private static final Logger log = LoggerFactory.getLogger(DeclarationIncidentController.class);
@@ -45,7 +56,32 @@ public class DeclarationIncidentController {
 
     @FXML
     public void initialize(){
+        idAnnuler.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            public void handle(MouseEvent event) {
+                backToHome(event);
+            }
+        });
+        idValider.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            public void handle(MouseEvent event) {
+                backToHome(event);
+            }
+        });
+    }
+    private void backToHome(MouseEvent event){
+        if (event.getButton() == MouseButton.PRIMARY) {
+            String fxmlFile = "/fxml/home.fxml";
+            FXMLLoader loader = new FXMLLoader();
+            try {
+                Stage stage = (Stage) idValider.getScene().getWindow();
+                Parent rootNode = (Parent) loader.load(getClass().getResourceAsStream(fxmlFile));
 
+                Scene scene = new Scene(rootNode);
+                stage.setScene(scene);
+                stage.show();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 }
--- src/main/java/fr/polytech/ihm/MainApp.java
+++ src/main/java/fr/polytech/ihm/MainApp.java
@@ -26,7 +26,7 @@ public class MainApp extends Application {
         Parent rootNode = (Parent) loader.load(getClass().getResourceAsStream(fxmlFile));
 
         log.debug("Showing JFX scene");
-        Scene scene = new Scene(rootNode, 600, 300);
+       Scene scene = new Scene(rootNode, 600, 300);
         scene.getStylesheets().add("/styles/styles.css");
 
         stage.setTitle("Hello JavaFX and Maven");
